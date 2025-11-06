63ea1bbeac9a74409d49cd5373f9e712cd418254
antaramotorid <antaramotorkalisari@gmail.com>
2025-11-07 04:47:58 +0700

    Update page.tsx

--- app/listings/[id]/page.tsx
+++ app/listings/[id]/page.tsx
@@ -3,7 +3,7 @@
 
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { supabase } from '@/lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
 
 type Listing = {
   id: string;
