23cd8a2d44699809b8f728bad9125d3e66f6a349
antaramotorid <antaramotorkalisari@gmail.com>
2025-11-11 03:58:50 +0700

    Update page.tsx

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -3,6 +3,7 @@
 
 import { useEffect, useMemo, useRef, useState } from "react";
 import { supabase } from "../../lib/supabaseClient";
+
 // ================== Helpers (static data) ==================
 const YEARS = (() => {
   const now = new Date().getFullYear();
